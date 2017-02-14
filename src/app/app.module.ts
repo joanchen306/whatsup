00dae1885f98b700a19ce2da72c1893a53284537
Brandi Van de Houten <Brandi Van de Houten>
2017-02-14 14:08:39 -0500

    Setup Ionic Cloud Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': '90d65db5'
+  }
+}
 
 @NgModule({
   declarations: [
@@ -11,7 +18,8 @@ import { Page2 } from '../pages/page2/page2';
     Page2
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
