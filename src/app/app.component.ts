54fa918a621cd80e123a98dcf9ceee02bc1318b0
Aaron Parry <aaronparry@lawn-143-215-90-196.lawn.gatech.edu>
2017-02-20 10:46:35 -0500

    maps are working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
+import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 import { MapPage } from '../pages/map/map';
 
@@ -22,6 +23,7 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
+      { title: 'WhatsApp', component: Page1 },
       { title: 'Page Two', component: Page2 },
       { title: 'Map', component: MapPage }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
 import { MyApp } from './app.component';
+import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
 import { MapPage } from '../pages/map/map';
 
@@ -14,6 +15,7 @@ const cloudSettings: CloudSettings = {
 @NgModule({
   declarations: [
     MyApp,
+    Page1,
     Page2,
     MapPage
   ],
@@ -24,6 +26,7 @@ const cloudSettings: CloudSettings = {
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    Page1,
     Page2,
     MapPage
   ],
