16dee81dba0128ffe71451c2d3d2f50f7577dd49
Brandi Van de Houten <Brandi Van de Houten>
2017-03-05 18:21:09 -0500

    Merged navigation branch into master

--- src/pages/mainpage/mainpage.ts
+++ src/pages/mainpage/mainpage.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, Slides } from 'ionic-angular';
+
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'main-page',
@@ -8,8 +10,25 @@ import { NavController } from 'ionic-angular';
 })
 export class MainPage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild(Slides) slides: Slides;
 
+  FRIENDS_SLIDE_INDEX: number = 0;
+  MAP_SLIDE_INDEX: number = 1;
+  EVENTS_LIST_SLIDE_INDEX: number = 2;
+
+  constructor(public navCtrl: NavController) {
   }
 
+  goToSlide(slideIndex) {
+    this.slides.lockSwipes(false);
+    this.slides.slideTo(slideIndex, 500);
+  }
+
+  onSlideDidChange() {
+    if (this.slides.getActiveIndex() == this.MAP_SLIDE_INDEX) {
+      this.slides.lockSwipes(true);
+    } else {
+      this.slides.lockSwipes(false);
+    }
+  }
 }
