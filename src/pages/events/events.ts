86588eee4d214e353d26ec669cd0a1dfcc4c1e15
Aaron Parry <aaronparry@Aarons-MacBook-Pro.local>
2017-03-05 23:33:05 -0500

    added dummy event data

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -7,9 +7,20 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'events.html'
 })
 export class EventsPage {
-
+  events: Array<{name:string, distance:number, description:string}>
   constructor(public navCtrl: NavController) {
-
+    this.events = [
+      {
+        name: "Esteban's Shindig",
+        distance: 1.1,
+        description: "Come celebrate your average Thursday night with Esteban and the crew. BYOB ofc"
+      },
+      {
+        name: "Miguel's Lunar Eclipse Get-together",
+        distance: 0.8,
+        description: "Come celebrate the lunar eclipse, they don't happen very often ya know!"
+      },
+    ];
   }
 
 }
