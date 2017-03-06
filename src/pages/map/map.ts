7c1bcbaabbb510a498cddd3a99143ec2d9fcf278
Brandi Van de Houten <Brandi Van de Houten>
2017-03-06 01:27:57 -0500

    Map initializes at users current location and a locate me button was added to the map.

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 
-import { NavController, Platform } from 'ionic-angular';
+import { NavController, Platform, ToastController } from 'ionic-angular';
 
 declare var google;
 
@@ -8,26 +8,120 @@ declare var google;
   selector: 'map-page',
   templateUrl: 'map.html'
 })
-export class MapPage {
+export class MapPage implements OnInit {
 
     @ViewChild('map') mapElement;
     map: any;
 
-    constructor(public navCtrl: NavController, public platform: Platform) {
-        platform.ready().then(() => {
-            this.initMap();
-        });
+    constructor(public navCtrl: NavController, public platform: Platform, public toastCtrl: ToastController) {
+
+    }
+
+    ngOnInit() {
+    }
+
+    ngAfterViewInit() {
+      this.initMap();
     }
 
     initMap() {
-      let latLng = new google.maps.LatLng(33.7756, -84.3963);
+      let lat = 35.000;
+      let long = -84.000;
+
+      let latLng = new google.maps.LatLng(lat, long);
 
       let mapOptions = {
         center: latLng,
         zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true,
+        zoomControl: true,
       };
 
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+      this.centerToMyLocation();
+
+      // Create the DIV to hold the control and call the CenterControl()
+      // constructor passing in this DIV.
+      var centerControlDiv = document.createElement('div');
+      this.findMeControl(centerControlDiv, this.map);
+
+      this.map.controls[google.maps.ControlPosition.BOTTOM_RIGHT].push(centerControlDiv);
+    }
+
+    centerToMyLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.centerMap.bind(this), this.showError);
+      } else {
+        //browser does not support geolocation 
+        this.presentToast("Browser does not support geolocation");
+      }
+    }
+
+    centerMap(position) {
+      let lat = position.coords.latitude;
+      let long = position.coords.longitude;
+
+      let latLng = new google.maps.LatLng(lat, long);
+
+      this.map.setCenter(latLng);
+    }
+
+    showError(error) {
+      switch(error.code) {
+        case error.PERMISSION_DENIED:
+          this.presentToast("User denied the request for Geolocation.");
+          break;
+        case error.POSITION_UNAVAILABLE:
+          this.presentToast( "Location information is unavailable.");
+          break;
+        case error.TIMEOUT:
+          this.presentToast("The request to get user location timed out.");
+          break;
+        case error.UNKNOWN_ERROR:
+          this.presentToast("An unknown error occurred.");
+          break;
+      }
+    }
+
+    findMeControl(findMeControlDiv, map) {
+      // Set CSS for the control border.
+      var findMeControlUI = document.createElement('div');
+      findMeControlUI.style.backgroundColor = '#fff';
+      findMeControlUI.style.border = '2px solid #fff';
+      findMeControlUI.style.borderRadius = '3px';
+      findMeControlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+      findMeControlUI.style.cursor = 'pointer';
+      findMeControlUI.style.marginBottom = '22px';
+      findMeControlUI.style.textAlign = 'center';
+      findMeControlUI.title = 'Click to find your location';
+      findMeControlDiv.appendChild(findMeControlUI);
+
+      // Set CSS for the control interior.
+      var findMeControlText = document.createElement('div');
+      findMeControlText.style.color = 'rgb(25,25,25)';
+      findMeControlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+      findMeControlText.style.fontSize = '16px';
+      findMeControlText.style.lineHeight = '38px';
+      findMeControlText.style.paddingLeft = '5px';
+      findMeControlText.style.paddingRight = '5px';
+      findMeControlText.innerHTML = 'ME';
+      findMeControlUI.appendChild(findMeControlText);
+
+      // Setup the click event listener
+      findMeControlText.addEventListener('click', this.findMeControlEventListener, false);
+    }
+
+    private findMeControlEventListener = (evt:Event) => {
+      this.centerToMyLocation();
+    }
+
+    presentToast(message) {
+      let toast = this.toastCtrl.create({
+        message: message,
+        duration: 3000
+      });
+
+      toast.present();
     }
 }
