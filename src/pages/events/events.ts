d00bcae1f78588f054e62417fe1c4feca3f78ba7
willchristian343 <wchristian6@gatech.edu>
2017-03-27 17:41:26 -0400

    Integrating Eventbrite

--- src/pages/events/events.ts
+++ src/pages/events/events.ts
@@ -1,26 +1,36 @@
 import { Component } from '@angular/core';
-
+import {EventProvider} from '../../providers/event-provider';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'event-list',
-  templateUrl: 'events.html'
+  templateUrl: 'events.html',
+  providers: [EventProvider]
 })
 export class EventsPage {
-  events: Array<{name:string, distance:number, description:string}>
-  constructor(public navCtrl: NavController) {
-    this.events = [
-      {
-        name: "Esteban's Shindig",
-        distance: 1.1,
-        description: "Come celebrate your average Thursday night with Esteban and the crew. BYOB ofc"
-      },
-      {
-        name: "Miguel's Lunar Eclipse Get-together",
-        distance: 0.8,
-        description: "Come celebrate the lunar eclipse, they don't happen very often ya know!"
-      },
-    ];
+  events = []
+
+
+  constructor(public navCtrl: NavController, private eventProvider: EventProvider) {
+
+    eventProvider.getJsonData().subscribe((res)=>{
+      this.events = res.events;
+      alert(res.events);
+    });
+
+    // [
+    //   {
+    //     name: "Esteban's Shindig",
+    //     distance: 1.1,
+    //     description: "Come celebrate your average Thursday night with Esteban and the crew. BYOB ofc"
+    //   },
+    //   {
+    //     name: "Miguel's Lunar Eclipse Get-together",
+    //     distance: 0.8,
+    //     description: "Come celebrate the lunar eclipse, they don't happen very often ya know!"
+    //   },
+    // ];
   }
+
 
 }
--- src/providers/event-provider.ts
+++ src/providers/event-provider.ts
@@ -11,37 +11,15 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class EventProvider {
-  data:any;
+
   constructor(public http: Http) {
-    this.data = null;
-    //console.log('Hello EventProvider Provider');
+    console.log("Event Provider")
   }
-
-  public getCityEvents(city) {
-    var event = {id: "", name: "", start: "", end: "", latitude: "", longitude: "", venue:""};
-    NativeStorage.getItem('event')
-      .then((data) => {
-        alert("data: " + JSON.stringify(data));
-        alert("data id: " + data.id);
-
-        // event = {
-        //   id = data.id,
-        //   name = data.name,
-        //   start = data.start,
-        //   end = data.end,
-        //   latitude = data.latitude,
-        //   longitude = data.longitude,
-        //   venue = data.venue
-        // };
+  public getJsonData(){
 
 
-      this.http.get("https://www.eventbriteapi.com/v3/events/search/?location.address=Atlanta&expand=organizer,venue&token=VMGQGYQUIO3IKNS75BD4").subscribe( data =>{
-        console.log(data);
-        //variable assignment
-        this.data = data;
-      },
-      error => {
-        console.log(error)
-      });
-      })
-}  }
+    return this.http.get('https://www.eventbriteapi.com/v3/events/search/?location.address=Atlanta&expand=organizer,venue&token=VMGQGYQUIO3IKNS75BD4').map(res => res.json().events);
+
+  }
+    //console.log('Hello EventProvider Provider');
+}
