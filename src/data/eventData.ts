d39c7a3005d5417b4e224d8df8518f717ae07846
Will Christian <wchristian6@gatech.edu>
2017-04-09 14:53:13 -0400

    adding event data

---
+++ src/data/eventData.ts
@@ -0,0 +1,24 @@
+export class EventData {
+    public events = [
+      {
+        name:'Easter Glass Blowing',
+        category: 'Art/Film',
+        start: "2:00pm",
+        end: "6:00pm",
+        promoter: "Decatur Glassblowing",
+        address: "250 Freeman Street, Decatur, Georgia 30030",
+        latitude: 33.772078,
+        longitude: -84.287371,
+      },
+      {
+        name:'Zuchero',
+        category: 'Music',
+        start: "8:00pm",
+        end: "10:00pm",
+        promoter: "Decatur Glassblowing",
+        address: "1374 West Peachtree Street, Atlanta, Georgia 30309",
+        latitude: 33.792034,
+        longitude: -84.387980,
+      }
+    ]
+}
