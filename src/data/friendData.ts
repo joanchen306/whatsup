686e7f6362462953a825cb9d0c4fdcb40430b275
ryanbrooks <r.brooks112@yahoo.com>
2017-04-09 10:41:29 -0400

    Fake friends working

--- src/data/friendData.ts
+++ src/data/friendData.ts
@@ -1,3 +1,64 @@
 export class FriendData {
-
+    public friends = [
+      {
+        name:'Brandi Van de Houten',
+        available:true,
+        latitude: 33.7737551,
+        longitude: -84.3921202,
+      },
+      {
+        name:'Aaron Parry',
+        available:true,
+        latitude: 33.7737551,
+        longitude: -84.3921202,
+      },
+      {
+        name:'Will Christian',
+        available:true,
+        latitude: 33.7737551,
+        longitude: -84.3921202,
+      },
+      {
+        name:'Joan Chen',
+        available:true,
+        latitude: 33.7737551,
+        longitude: -84.3921202,
+      },
+      {
+        name:'Bob Waters',
+        available:true,
+        latitude: 33.7737551,
+        longitude: -84.3921202,
+      },
+      {
+        name:'Kelly Fitzpatrick',
+        available:true,
+        latitude: 33.7737551,
+        longitude: -84.3921202,
+      },
+      {
+        name:'Dawn Sabados',
+        available:true,
+        latitude: 33.7737551,
+        longitude: -84.3921202,
+      },
+      {
+        name:'Graham Miles',
+        available:true,
+        latitude: 33.7737551,
+        longitude: -84.3921202,
+      },
+      {
+        name:'Branson Dupuy',
+        available:true,
+        latitude: 33.7737551,
+        longitude: -84.3921202,
+      },
+      {
+        name:'Tengfei Lin',
+        available:true,
+        latitude: 33.7737551,
+        longitude: -84.3921202,
+      }
+    ]
 }
--- src/pages/friends/friends.ts
+++ src/pages/friends/friends.ts
@@ -3,11 +3,12 @@ import {NavController, NavParams} from "ionic-angular";
 import {Observable} from "rxjs/Rx";
 import {FacebookService} from "../../services/facebook.service";
 import {Http} from "@angular/http";
+import {FriendData} from "../../data/friendData";
 
 @Component({
   selector: 'friends-list',
   templateUrl: 'friends.html',
-  providers: [FacebookService]
+  providers: [FriendData]
 })
 export class FriendsPage {
   selectedItem:any;
@@ -20,51 +21,20 @@ export class FriendsPage {
   public friendsObservable:Observable<any[]>;
   @Input() userId:string;
 
-  private accessToken = 'EAACEdEose0cBAMR4BmdM3kh6uYPFAYVOPZAjHZCrBB19Yaee2B9ZAialtVIfUTXaZBMpdV18W3IbLCXKXnShPmvYYWYZB4aus0vFlZCMIiFtHErsfkMAjgNQaRKf5BZCP5U5AZCnZBlozb14ObFJkoaEt2NjlUR5nuwza4eK6J6MAyKQb5xRSuAoC';
-
-  private graphUrl = `https://graph.facebook.com/v2.8/?access_token=${this.accessToken}/`;
-  private friendsQuery = '/friends';
-
-  constructor(public navCtrl:NavController, public navParams:NavParams, private http:Http) {
+  constructor(public navCtrl:NavController, public navParams:NavParams, private friendData:FriendData) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
-    let url = this.graphUrl + "10211612897815532" + this.friendsQuery;
-
-    this.http
-      .get(url)
-      .map(response => response.json())
-      .subscribe(data => {
-        alert("Data: " + data);
+    this.friends = [];
+    for (let i = 0; i < friendData.friends.length; i++) {
+      this.friends.push({
+        name: friendData.friends[i].name,
+        active: friendData.friends[i].available,
+        latitude: friendData.friends[i].latitude,
+        longitude: friendData.friends[i].longitude
       });
-
-    // // Let's populate this page with some filler content for funzies
-    // this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    // 'american-football', 'boat', 'bluetooth', 'build'];
-    //
-    // this.fnames = ['Aaron','Ryan','John','Will','Alice','Brandi','Joan','Mark',
-    // 'Andrew','Austin','Luke','Matt','Lauren','Bianca'];
-    // this.lnames = ['Parry','James','Goldstein','Brooks','Chen','Christian','Lee','Smith'];
-    // this.tf = [true,false]
-    // this.friends = [];
-    // for (let i = 1; i < 35; i++) {
-    //   this.friends.push({
-    //     name: this.fnames[Math.floor(Math.random() * this.fnames.length)] + " " + this.lnames[Math.floor(Math.random() * this.lnames.length)],
-    //     active: this.tf[Math.floor(Math.random() * this.tf.length)],
-    //     distance:Math.random()*10,
-    //     icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-    //   });
-    // }
+    }
   }
-
-  mapFriends = (friend) => {
-    this.friends.push({
-      name: friend.name,
-      active: this.tf[Math.floor(Math.random() * this.tf.length)],
-      distance: Math.random() * 10,
-      icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-    });
-  };
 
   // itemTapped(event, item) {
   //   // That's right, we're pushing to ourselves!
