ff30eda1fdf6c1717f39f6eaa397f942fabe16c6
dtbanquet <tamil@dramatech.org>
2017-04-11 15:15:30 -0400

    Revert "COLOR CHANGE"
    This reverts commit a5f3707162e1917277934c6b3523c1d981733e25.

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -126,7 +126,7 @@ export class MapPage implements OnInit {
           "elementType": "all",
           "stylers": [
             {
-              "color": "#221638"
+              "color": "#222638"
             }
           ]
         },
